7936f36ea6f83f1671fcbaae3ab4d24924918d6e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-08 16:53:25 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a;
+	string col[5] = { "Зелено", "Красно", "Желто", "Бело", "Черно" };
+	string anml[12] = { " крысы", " коровы", " тигра", " зайца", " дракона", " змеи", " лошади", " овцы", " обезьяны", " курицы", " собаки", " свиньи" };
+
+	cout << "Введите год." << endl;
+	cin >> a;
+	
+
+	
+	system("pause");
+	return 0;
+}
