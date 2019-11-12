ef9e744377b7b64e0a69d6394bca4603ba781110
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-12 12:26:55 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -6,27 +6,28 @@ using namespace std;
 int main()
 {
 	setlocale(0, "");
-	int a, ry = (a - 1984);
 	string col[5] = { "Зелено", "Красно", "Желто", "Бело", "Черно" };
 	string anml[12] = { " крысы", " коровы", " тигра", " зайца", " дракона", " змеи", " лошади", " овцы", " обезьяны", " курицы", " собаки", " свиньи" };
+	int a;
 
-	cout << "Введите год." << endl;
+	cout << "Введите нужный вам год." << endl;
 	cin >> a;
-	
-	cout << col[ry / 12];
-	
-    if(ry % 12 == 2 || ry % 12 == 3 || ry % 12 == 4)
-    {
-    cout << "go ";
-    }
-    else
-    {
-     cout << "i ";
-    }
-    cout << anml[ry % 12];
 
+	int ry = (a - 1984);
 
 	
+	cout << a << " год - год " << col[ry / 12];
+
+	if (ry % 12 == 2 || ry % 12 == 3 || ry % 12 == 4)
+	{
+		cout << "го ";
+	}
+	else
+	{
+		cout << "й ";
+	}
+	cout << anml[ry % 12] << endl;
+
 	system("pause");
 	return 0;
 }
